a7364789ca2da6a022427f759557a7f731d15527
Game-store <duboy36@gmail.com>
2022-07-27 23:18:51 -0300

    final

--- js/api.js
+++ js/api.js
@@ -1,46 +1,75 @@
-
-
 const API_URL = 'https://api.mercadolibre.com/'
 const endpointProductos = 'sites/MLA/search'
 
+
+
 const traerDatosBaseDeDatos = () => {
-    fetch(API_URL+endpointProductos+'?nickname=METAJUEGO')
-    .then((response)=> response.json())
-    .then ((data) =>{
-        productos = data.results;
-        console.log(productos)
-        generarCards(productos);
-       
-    })
-    
+fetch(API_URL+endpointProductos+'?nickname=SONY+ARGENTINA')
+.then((response)=> response.json())
+.then ((data) =>{
+productos = data.results;
+generarCards(productos);
+
+
+})
 }
 
 
 const generarCards = (arrayDeProductos) => {
-    
-    let cards = '';
-    arrayDeProductos.forEach((productos) => {
-        cards += `<div class="col mb-5">
-        <div class="card h-100 images">                     
-        <img class="card-img-to images"  src="${productos.thumbnail}"alt="..." />                  
-        <div class="card-body p-4 coloor">
-        <div class="text-center ">
-        <h5 class="fw-bolder title" >${productos.title}</h5><p class="price">$${productos.price}</p>
-        </div>
-        </div>
-        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-        <div class="text-center verproducto"><a class="btn btn-outline-dark mt-auto"  href="${productos.permalink}">Ver producto</a></div>
-        </div>
-        </div>b                                                                                                                                                                                                                                                                               
-        </div>`;
-        
-    }) 
+
+let cards = '';
+arrayDeProductos.forEach((productos) => {
+cards += `<div class="producapi">
+<div class="card images-container">                     
+<img class="card-img-to images"  src="${productos.thumbnail}"alt="..." />                
+<div class="">
+<div class="text-center ">
+<h5 class="titulo-api" >${productos.title}</h5><p class="precio">$${productos.price}</p>
+
+</div>
+</div>
+<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+<div class="text-center verproducto"><a class="btn btn-outline-dark mt-auto boton-api"  href="${productos.permalink}">Ver producto</a></div>
+</div>
+</div>                                                                                                                                                                                                                                                                            
+</div>`;
+
+}) 
 
 
 
 document.getElementById("card-container-api").innerHTML = cards;
 }
 
+traerDatosBaseDeDatos()
+
+const masInformacion = document.getElementById('ps5-info');
+
+masInformacion.onclick =() =>{
+    location.href='https://www.playstation.com/es-ar/ps5/'
+}
 
 
-traerDatosBaseDeDatos();
+const ladt = document.getElementById('click-ladt');
+
+ladt.onclick =() =>{
+    location.href='https://www.playstation.com/es-ar/games/the-last-of-us-part-i/'
+}
+
+//*Evento en el bt de enviar formulario*//
+const enviar = document.getElementById("enviar");
+
+enviar.onclick = () =>{
+const nombre = document.getElementById("nombre").value;
+const mail = document.getElementById("mail").value;
+const apellido = document.getElementById("apellido").value;
+localStorage.setItem("nombre", JSON.stringify((nombre)))
+localStorage.setItem("apellido", JSON.stringify((apellido)))
+localStorage.setItem("mail", JSON.stringify((mail)))
+Swal.fire(
+'!Su mensaje se envio con exito!',
+'¡Muchas gracias!',
+'success'
+)
+
+}
--- js/scripts.js
+++ js/scripts.js
@@ -14,338 +14,324 @@ let buttonDOM = [];
 
 class UI {
 
-	detalleProducto(id){
-		const filtroDato = productos.filter(item => item.id == id)
-		let result = ""
-		filtroDato.forEach(producto => {
-			result += `
-			<article class="detalle-grid">
-				<img src=${producto.image} alt="${producto.title}" class="img-fluid">
-				<div class="detalles-content">
-					<h3>${producto.title}</h3>
-					<div class="rating">
-						<span>
-							<i class="bx bxs-star"></i>
-						</span>
-						<span>
-							<i class="bx bxs-star"></i>
-						</span>
-						<span>
-							<i class="bx bxs-star"></i>
-						</span>
-						<span>
-							<i class="bx bxs-star"></i>
-						</span>
-						<span>
-							<i class="bx bx-star"></i>
-						</span>
-					</div>
-						<p class="price"><b>Precio: </b> $${producto.price}</p>
-						<p class="description">
-							<b>Descripcion: </b> <span>HOLA</span>
-						</p>
-						<p class="description">
-							<span>Hola MUNDO </span>
-						</p>
-						<div class="bottom">
-							<div class="btn__group">
-							<button class="btn " data-id=${producto.id}>Añadir carrito</button>
-							</div>
-						</div>
-				</div>
-			</article>
-			`
-		});
-		detalles.innerHTML = result;
-	}
+detalleProducto(id){
+const filtroDato = productos.filter(item => item.id == id)
+let result = ""
+filtroDato.forEach(producto => {
+result += `
+<article class="detalle-grid">
+<img src=${producto.image} alt="${producto.title}" class="img-fluid">
+<div class="detalles-content">
+<h3>${producto.title}</h3>
+<div class="rating">
+<span>
+<i class="bx bxs-star"></i>
+</span>
+<span>
+<i class="bx bxs-star"></i>
+</span>
+<span>
+<i class="bx bxs-star"></i>
+</span>
+<span>
+<i class="bx bxs-star"></i>
+</span>
+<span>
+<i class="bx bx-star"></i>
+</span>
+</div>
+<p class="price"><b>Precio: </b> $${producto.price}</p>
+<p class="description">
+<b>Descripcion: </b> <span>Hasta 120 fps con salida de 120 Hz : disfrute de un juego fluido y fluido con una alta velocidad de fotogramas de hasta 120 fps para juegos compatibles, con soporte para salida de 120 Hz en pantallas 4K.</span>
+</p>
+<p class="description">
+<span>Tempest 3D AudioTech : sumérjase en paisajes sonoros donde se siente como si el sonido viniera de todas las direcciones. A través de sus auriculares o parlantes de TV, su entorno realmente cobra vida con Tempest 3D AudioTech en juegos compatibles.</span>
+</p>
+<div class="bottom">
+<div class="btn__group ">
+<button class="btn addToCart " data-id=${producto.id}>Agregar al carrito</button>
+</div>
+</div>
+</div>
+</article>
+`
+});
+detalles.innerHTML = result;
+}
 
-	renderProductos(productos){
-		let result = ""
-		productos.forEach((producto) =>{
-			result += `
-			<div class="producto">
-			<div class="image__container">
-			<img src=${producto.image} alt="">
-		</div>
-          <div class="producto__footer">
-            <h1>${producto.title}</h1>
-            <div class="rating">
-              <span>
-                <i class="bx bxs-star"></i>
-              </span>
-              <span>
-                <i class="bx bxs-star"></i>
-              </span>
-              <span>
-                <i class="bx bxs-star"></i>
-              </span>
-              <span>
-                <i class="bx bxs-star"></i>
-              </span>
-              <span>
-                <i class="bx bx-star"></i>
-              </span>
-            </div>
-            <div class="price">$${producto.price}</div>
-          </div>
-          <div class="bottom">
-            <div class="btn__group">
-              <button class="btn addToCart" data-id=${producto.id}>Añadir carrito</button>
-              <a href="producto-detalles.html?id=${producto.id}" class="btn view">Vista</a>
-            </div>
-          </div>
-        </div>
-				`
-		});
-		productoDOM.innerHTML = result
-	}
+renderProductos(productos){
+let result = ""
+productos.forEach((producto) =>{
+result += `
+<div class="producto">
+    <div class="image__container">
+        <img src=${producto.image} alt="">
+    </div>
+    <div class="producto__footer">
+    <h1>${producto.title}</h1>
+    <div class="rating">
+<span>
+<i class="bx bxs-star"></i>
+</span>
+<span>
+<i class="bx bxs-star"></i>
+</span>
+<span>
+<i class="bx bxs-star"></i>
+</span>
+<span>
+<i class="bx bxs-star"></i>
+</span>
+<span>
+<i class="bx bx-star"></i>
+</span>
+</div>
+<div class="price">$${producto.price}</div>
+</div>
+<div class="bottom">
+<div class="btn__group">
+<button class="btn addToCart" data-id=${producto.id}>Anadir carrito</button>
+<a href="producto-detalles.html?id=${producto.id}" class="btn view">Vista</a>
+</div>
+</div>
+</div>
+`
+});
+productoDOM.innerHTML = result
+}
 
-	getButtons(){
-		const buttons = [...document.querySelectorAll(".addToCart")];
-		buttonDOM = buttons;
-		buttons.forEach((button)=> {
-			const id = button.dataset.id;
-			const inCart = carrito.find(item => item.id === parseInt(id, 10));
-			
-			if(inCart){
-				button.innerHTML = "En el carrito";
-				button.disabled = true;
-			}
-			button.addEventListener("click", e =>{
-				e.preventDefault();
-				e.target.innerHTML = "En el carrito";
-				e.target.disabled = true;
-				Swal.fire({
-					position: 'top-end',
-					icon: 'success',
-					title: 'Agregaste un articulo al carrito',
-					showConfirmButton: false,
-					timer: 1500
-				  })
-				
+getButtons(){
 
-				// GET productos al carrito
-				const carritoItem = {...Storage.getProductos(id), cantidad: 1}
+const buttons = [...document.querySelectorAll(".addToCart")];
+buttonDOM = buttons;
+buttons.forEach((button)=> {
 
-				//agregamos el producto al carrito
-				carrito = [...carrito, carritoItem]
+const id = button.dataset.id;
+const inCart = carrito.find(item => item.id === parseInt(id, 10));
 
-				//Guardamos el carrito al localstorage
-				Storage.saveCart(carrito)
+if(inCart){
 
-				//Set cart values
-				this.setItemValues(carrito)
-				this.addCarritoItem(carritoItem)
-			
-			})
-		})
-	}
+}
+button.addEventListener("click", e =>{
 
-	setItemValues(carrito){
-		let tempTotal = 0;
-		let itemTotal = 0;
-		carrito.map(item => {
-			tempTotal += item.price * item.cantidad;
-			itemTotal += item.cantidad;
-		});
-		carritoTotal.innerText = parseFloat(tempTotal.toFixed(2));
-		itemTotales.innerText = itemTotal
-	}
+e.preventDefault();
+e.target.innerHTML = "En el carrito";
+e.target.disabled = true;
 
-	addCarritoItem({image, price, title, id}){
-		const div = document.createElement("div")
-		div.classList.add("carrito__item")
 
-		div.innerHTML = `
-		<img src=${image} alt=${title}>
-		<div>
-			<h3>${title}</h3>
-			<p class="price">$${price}</p>
-		</div>
-		<div>
-			<span class="increase" data-id=${id}>
-				<i class="bx bxs-up-arrow"></i>
-			</span>
-			<p class="item__cantidad">1</p>
-			<span class="decrease" data-id=${id}>
-				<i class="bx bxs-down-arrow"></i>
-			</span>
-		</div>
-		<div>
-			<span class="remove__item" data-id=${id}>
-				<i class="bx bx-trash"></i>
-			</span>
-		</div>
-		`
-		carritoCenter.appendChild(div)
-	}
-	show(){
-		carritoDOM.classList.add("show")
-		overlay.classList.add("show")
-	}
-	hide(){
-		carritoDOM.classList.remove("show")
-		overlay.classList.remove("show")
-	}
-	setAPP(){
-		carrito = Storage.getCart()
-		this.setItemValues(carrito)
-		this.populate(carrito)
-		openCarrito.addEventListener("click", this.show)
-		closeCarrito.addEventListener("click", this.hide)
-	}
-	populate(carrito){
-		carrito.forEach(item => this.addCarritoItem(item))
-	}
-	cartLogic(){
-		clearCarritoBtn.addEventListener("click", () =>{
-			this.clearCarrito()
-			this.hide()
-		});
 
-		carritoCenter.addEventListener("click", e =>{
-			const target = e.target.closest("span")
-			const targetElement = target.classList.contains("remove__item");
-			console.log(target)
-			console.log(targetElement)
-			if(!target) return
-			if(targetElement){
-				const id = parseInt(target.dataset.id);
-				this.removeItem(id)
-				carritoCenter.removeChild(target.parentElement.parentElement)
-			}else if(target.classList.contains("increase")){
-				const id = parseInt(target.dataset.id, 10);
-				let tempItem = carrito.find(item => item.id === id);
-				tempItem.cantidad++;
-				Storage.saveCart(carrito)
-				this.setItemValues(carrito)
-				target.nextElementSibling.innerText = tempItem.cantidad
-			}else if(target.classList.contains("decrease")){
-				const id = parseInt(target.dataset.id, 10);
-				let tempItem = carrito.find(item => item.id === id);
-				tempItem.cantidad--;
 
-				if(tempItem.cantidad > 0){
-					Storage.saveCart(carrito);
-					this.setItemValues(carrito);
-					target.previousElementSibling.innerText = tempItem.cantidad;
-				}else{
-					this.removeItem(id);
-					carritoCenter.removeChild(target.parentElement.parentElement)
-				}
-			}
-		});
-	}
-	clearCarrito(){
-		const cartItems = carrito.map(item => item.id)
-		cartItems.forEach(id => this.removeItem(id))
+const carritoItem = {...Storage.getProductos(id), cantidad: 1}
 
-		while(carritoCenter.children.length > 0){
-			carritoCenter.removeChild(carritoCenter.children[0])
-		}
-	}
-	removeItem(id){
-		carrito = carrito.filter(item => item.id !== id);
-		this.setItemValues(carrito)
-		Storage.saveCart(carrito)
-		let button = this.singleButton(id);
-		if(button){
-			button.disabled = false;
-			button.innerText = "Añadir carrito"
-		}
-	}
-	singleButton(id){
-		return buttonDOM.find(button => parseInt(button.dataset.id) === id)
-	}
+carrito = [...carrito, carritoItem]
+
+
+Storage.saveCart(carrito)
+
+
+this.setItemValues(carrito)
+this.addCarritoItem(carritoItem)
+
+})
+
+})
+}
+
+setItemValues(carrito){
+let tempTotal = 0;
+let itemTotal = 0;
+carrito.map(item => {
+tempTotal += item.price * item.cantidad;
+itemTotal += item.cantidad;
+});
+carritoTotal.innerText = parseFloat(tempTotal.toFixed(2));
+itemTotales.innerText = itemTotal
+}
+
+addCarritoItem({image, price, title, id}){
+const div = document.createElement("div")
+div.classList.add("carrito__item")
+
+div.innerHTML = `
+<img src=${image} alt=${title}>
+<div>
+<h3>${title}</h3>
+<p class="price">$${price}</p>
+</div>
+<div>
+<span class="increase" data-id=${id}>
+<i class="bx bxs-up-arrow"></i>
+</span>
+<p class="item__cantidad">1</p>
+<span class="decrease" data-id=${id}>
+<i class="bx bxs-down-arrow"></i>
+</span>
+</div>
+<div>
+<span class="remove__item" data-id=${id}>
+<i class="bx bx-trash"></i>
+</span>
+</div>
+`
+carritoCenter.appendChild(div)
+}
+show(){
+carritoDOM.classList.add("show")
+overlay.classList.add("show")
+}
+hide(){
+carritoDOM.classList.remove("show")
+overlay.classList.remove("show")
+}
+setAPP(){
+carrito = Storage.getCart()
+this.setItemValues(carrito)
+this.populate(carrito)
+openCarrito.addEventListener("click", this.show)
+closeCarrito.addEventListener("click", this.hide)
+}
+populate(carrito){
+carrito.forEach(item => this.addCarritoItem(item))
+
+}
+cartLogic(){
+clearCarritoBtn.addEventListener("click", () =>{
+this.clearCarrito()
+this.hide()
+});
+
+carritoCenter.addEventListener("click", e =>{
+
+const target = e.target.closest("span")
+const targetElement = target.classList.contains("remove__item");
+
+if(!target) return
+if(targetElement){
+const id = parseInt(target.dataset.id);
+this.removeItem(id)
+carritoCenter.removeChild(target.parentElement.parentElement)
+}else if(target.classList.contains("increase")){
+const id = parseInt(target.dataset.id, 10);
+let tempItem = carrito.find(item => item.id === id);
+tempItem.cantidad++;
+Storage.saveCart(carrito)
+this.setItemValues(carrito)
+target.nextElementSibling.innerText = tempItem.cantidad
+}else if(target.classList.contains("decrease")){
+const id = parseInt(target.dataset.id, 10);
+let tempItem = carrito.find(item => item.id === id);
+tempItem.cantidad--;
+
+if(tempItem.cantidad > 0){
+Storage.saveCart(carrito);
+this.setItemValues(carrito);
+target.previousElementSibling.innerText = tempItem.cantidad;
+}else{
+this.removeItem(id);
+carritoCenter.removeChild(target.parentElement.parentElement)
+}
+}
+});
+}
+clearCarrito(){
+const cartItems = carrito.map(item => item.id)
+cartItems.forEach(id => this.removeItem(id))
+
+while(carritoCenter.children.length > 0){
+carritoCenter.removeChild(carritoCenter.children[0])
+}
+}
+removeItem(id){
+carrito = carrito.filter(item => item.id !== id);
+this.setItemValues(carrito)
+Storage.saveCart(carrito)
+let button = this.singleButton(id);
+if(button){
+button.disabled = false;
+button.innerText = "Aadir carrito"
+}
+}
+singleButton(id){
+return buttonDOM.find(button => parseInt(button.dataset.id) === id)
+
+}
 }
 
 
 
 class Storage {
-	static saveProduct(obj){
-		localStorage.setItem("productos", JSON.stringify(obj))
-	}
-	static saveCart(carrito){
-		localStorage.setItem("carrito", JSON.stringify(carrito))
-	}
-	static getProductos(id){
-		const producto = JSON.parse(localStorage.getItem("productos"))
-		return producto.find(product =>product.id === parseFloat(id, 10))
-	}
-	static getCart(){
-		return localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : [];
-	}
+
+static saveProduct(obj){
+localStorage.setItem("productos", JSON.stringify(obj))
+}
+static saveCart(carrito){
+localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+static getProductos(id){
+const producto = JSON.parse(localStorage.getItem("productos"))
+return producto.find(product =>product.id === parseFloat(id, 10))
+}
+static getCart(){
+return localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : [];
+}
 }
 
 class Productos {
-  async getProductos() {
-    try {
-			const result = await fetch("productos.json")
-			const data = await result.json()
-			const productos = data.items
-			return productos
-		}catch(err){
-			console.log(err)
-		}
-  }
+async getProductos() {
+try {
+const result = await fetch("productos.json")
+const data = await result.json()
+const productos = data.items
+return productos
+}catch(err){
+console.log(err)
+}
+}
 }
 
 let category = "";
 let productos  = [];
 
 function categoryValue(){
-	const ui = new UI();
+const ui = new UI();
 
-	category = document.getElementById("category").value
-	if(category.length > 0){
-		const producto = productos.filter(regx => regx.category === category)
-		ui.renderProductos(producto)
-		ui.getButtons();
-	}else{
-		ui.renderProductos(productos)
-		ui.getButtons();
-	
-	}
+category = document.getElementById("category").value
+if(category.length > 0){
+const producto = productos.filter(regx => regx.category === category)
+ui.renderProductos(producto)
+ui.getButtons();
+}else{
+
+    ui.renderProductos(productos)
+    ui.getButtons();
 }
+}
+
+
 
 const query = new URLSearchParams(window.location.search)
 let id = query.get('id')
 
 document.addEventListener("DOMContentLoaded", async () =>{
-	const productosLista = new Productos();
-	const ui = new UI();
+const productosLista = new Productos();
+const ui = new UI();
 
-	ui.setAPP()
+ui.setAPP()
 
-	productos = await productosLista.getProductos()
-	if(id){
-		ui.detalleProducto(id)
-		Storage.saveProduct(productos)
-		ui.getButtons();
-		ui.cartLogic();
-	}else{
-		ui.renderProductos(productos)
-		Storage.saveProduct(productos)
-		ui.getButtons();
-		ui.cartLogic();
-	}
+productos = await productosLista.getProductos()
+if(id){
+ui.detalleProducto(id)
+Storage.saveProduct(productos)
+ui.getButtons();
+ui.cartLogic();
+
+}else{
+ui.renderProductos(productos)
+Storage.saveProduct(productos)
+ui.getButtons();
+ui.cartLogic();
+}
 })
 
-//*Evento en el bt de enviar formulario*//
-const enviar = document.getElementById("enviar");
 
-enviar.onclick = () =>{
-const nombre = document.getElementById("nombre").value;
-const mail = document.getElementById("mail").value;
-const apellido = document.getElementById("apellido").value;
-localStorage.setItem("nombre", JSON.stringify((nombre)))
-localStorage.setItem("apellido", JSON.stringify((apellido)))
-localStorage.setItem("mail", JSON.stringify((mail)))
-Swal.fire(
-  '!Su mensaje se envio con exito!',
-  '¡Muchas gracias!',
-  'success'
-)
-
-}
 
